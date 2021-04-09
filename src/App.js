4862e3aad38d6ef53119e0d008a7258b88ebada8
Michael Ryan <michaelryan2895@gmail.com>
2021-04-08 20:56:03 -0400

    lab06

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import React from 'react';
+import axios from 'axios';
+import City from './City.js';
+import Search from './Search.js';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state={
+      haveWeSearchedYet: false,
+      citySearchedFor: '',
+    };
+  }
+
+  handleShowSearch = () => {
+    this.setState({haveWeSearchedYet: false});
+  }
+
+  handleSearch = async(cityResult) => {
+
+    let locationResponseData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${cityResult}&format=json`);
+    console.log(locationResponseData);
+    this.setState({
+      haveWeSearchedYet: true,
+      citySearchedFor: cityResult,
+      locationData: locationResponseData.data[0]
+    });
+  }
   render() {
     return(
       <>
         <h1>City Explorer</h1>
+        {this.state.haveWeSearchedYet ?
+        <City handleShowSearch={this.handleShowSearch} cityData={this.state.locationData} /> :
+        <Search handleSearch={this.handleSearch} />}
       </>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.textInput = React.createRef();
+    }
+    handleFormSubmitted = (event) => {
+        event.preventDefault();
+
+        this.props.handleSearch(this.textInput.current.value);
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmitted}>
+                <input type="text" ref={this.textInput} />
+                <button type="submit">Explore</button>
+            </form>
+        )
+    }
+}
+
+export default Search;
